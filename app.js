ef67dbdadfb2ac4418bd7d0b67c2afeff6bd49d3
Stasiperez85 <Stasiperez85@gmail.com>
2022-09-21 21:42:21 -0600

    start javascript adding questions

---
+++ app.js
@@ -0,0 +1,7 @@
+var questions = [
+    new Questions("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
+    new Questions("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Questions("There are _____ main components of object oriented programming.", ["1", "6", "3", "4"], "4"),
+    new Questions("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+    new Questions("MVC is a ___.", ["Lanuage", "Library", "Framework", "All"], "Framework")
+];
